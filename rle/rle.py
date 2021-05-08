220ee087fee52e1ee9a2db4c36f11e249d1dd5c2
glukky <blackmamba05@yandex.ru>
2021-05-08 20:16:10 +0300

    Fix indentation to spaces

--- rle/rle.py
+++ rle/rle.py
@@ -1,30 +1,38 @@
+#!/usr/bin/env python3
+
 import sys
 
-D = [str(x) for x in range(0, 10)]
+_DIGITS = [str(x) for x in range(0, 10)]
 
 
 def mystr(symbol, count):
-	if symbol is None:
-		return ''
-	if count == 1:
-		return symbol
-	list_num =[]
-	while count > 0:
-		number = count % 10
-		count = count // 10
-		list_num.append(D[number])
-	return symbol + ''.join(list_num[::-1]) 
+    if symbol is None:
+        return ''
+    if count == 1:
+        return symbol
+    list_num = []
+    while count > 0:
+        number = count % 10
+        count = count // 10
+        list_num.append(_DIGITS[number])
+    return symbol + ''.join(list_num[::-1]) 
 
-with open(sys.argv[1], 'r') as f:
-	with open(sys.argv[2], 'w') as f_out:
-		next_symbol = f.read(1)
-		symbol = None
-		count = 1
-		while next_symbol:
-			if symbol == next_symbol:
-				count += 1
-			else:
-				f_out.write(mystr(symbol, count))
-				count = 1
-				symbol = next_symbol
-			next_symbol = f.read(1)
+
+def main():
+    with open(sys.argv[1], 'r') as f:
+        with open(sys.argv[2], 'w') as f_out:
+            next_symbol = f.read(1)
+            symbol = None
+            count = 1
+            while next_symbol:
+                if symbol == next_symbol:
+                    count += 1
+                else:
+                    f_out.write(mystr(symbol, count))
+                    count = 1
+                    symbol = next_symbol
+                next_symbol = f.read(1)
+
+
+if __name__ == '__main__':
+    main()
