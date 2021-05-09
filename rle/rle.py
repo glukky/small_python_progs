1277e64e36fda7c804947414096a174e5807625e
glukky <blackmamba05@yandex.ru>
2021-05-09 11:37:46 +0300

    add tests

--- rle/rle.py
+++ rle/rle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+#import sys
 
 _DIGITS = [str(x) for x in range(0, 10)]
 
@@ -18,20 +18,44 @@ def mystr(symbol, count):
     return symbol + ''.join(list_num[::-1]) 
 
 
-def main():
-    with open(sys.argv[1], 'r') as f:
-        with open(sys.argv[2], 'w') as f_out:
-            next_symbol = f.read(1)
-            symbol = None
+def rle(input_str):
+    symbol = None
+    count = 1
+    output_str = ''
+    # print(input_str)
+    for next_symbol in input_str:
+        if symbol == next_symbol:
+            count += 1
+        else:
+            chunk = mystr(symbol, count)
+            # print('    ', chunk)
+            output_str += chunk
             count = 1
-            while next_symbol:
-                if symbol == next_symbol:
-                    count += 1
-                else:
-                    f_out.write(mystr(symbol, count))
-                    count = 1
-                    symbol = next_symbol
-                next_symbol = f.read(1)
+            symbol = next_symbol
+    chunk = mystr(symbol, count)
+    # print('    ', chunk)
+    output_str += chunk
+    
+    # TODO(glukky): fixme
+    return output_str
+
+
+def test_rle(input_str, output_str):
+    conv_input = rle(input_str)
+    assert conv_input == output_str, 'Test `{}` != `{}` failed'.format(conv_input, output_str)
+
+
+def test_all_rle():
+    test_rle('', '')
+    test_rle('AAAABBBCCCCC', 'A4B3C5')
+    test_rle('AAAABBB', 'A4B3')
+    test_rle('AAAA', 'A4')
+    test_rle('ABC', 'ABC')
+    print('All tests are OK')
+
+
+def main():
+    test_all_rle()
 
 
 if __name__ == '__main__':
