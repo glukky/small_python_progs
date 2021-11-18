2529fd0f9c3fb66646ce1bb3277b9fdb30e43247
Mikhail Veltishchev <mvel@yandex-team.ru>
2021-11-19 02:44:00 +0300

    A little bit better track cleaner and track merger tool

--- trek_cleaner/trek_cleaner.py
+++ trek_cleaner/trek_cleaner.py
@@ -1,45 +1,85 @@
-import xml.etree.ElementTree as ET
-from geopy.distance import geodesic
+import os
 import sys
 
-#Убираем префиксы пространств имен в xml-документе при выводе в файл
-ET.register_namespace('', 'http://www.topografix.com/GPX/1/1')
+import xml.etree.ElementTree as ET
+from geopy import distance as gd
 
-#Формируем дерево элементов
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
 
-#Найдём lat, lon и ele первой точки трека
-#Е - родительский элемент для всех точек трека
-trkpt_0 = root.find('{*}trk/{*}trkseg/{*}trkpt')
-E = root.find('{*}trk/{*}trkseg')
-latitude_0 = trkpt_0.get('lat')
-longitude_0 = trkpt_0.get('lon')
-elevation_0 = trkpt_0.find('{*}ele').text
+_DISTANCE_TRESHOLD = 20
 
-# Удадим из дерева элементов все точки, расстояние можду которыми
-# меньше 15м. Исключение - перепад высоты больше 1м
-for elem in root.findall('{*}trk/{*}trkseg/{*}trkpt'):
-    latitude = elem.get('lat')
-    longitude = elem.get('lon')
-    elevation = elem.find('{*}ele').text
-    ele_delta = abs(float(elevation) - float(elevation_0))
-    if latitude == latitude_0 and longitude == longitude_0 or ele_delta > 1:
-        elevation_0 = elevation
-        continue
+_NS = 'http://www.topografix.com/GPX/1/1'
 
-    #Считаем расстояние между точками
-    mark1 = (latitude_0, longitude_0)
-    mark2 = (latitude, longitude)
-    dist = geodesic(mark1, mark2, ellipsoid='WGS-84').m
-    if dist < 15:
-        E.remove(elem)
-    else:
-        latitude_0 = latitude
-        longitude_0 = longitude
-        elevation_0 = elevation
 
-#Записываем новый xml-документ на основе полученного дерева
-file_name = sys.argv[1]
-new_file_name = file_name[:-4] + '_new.gpx'
-tree.write(new_file_name, encoding="UTF-8")
+def _get_elevation(point):
+    elevation_elem = point.find('{*}ele')
+    if elevation_elem is not None:
+        return float(elevation_elem.text)
+    return 0
+
+
+def main():
+    ET.register_namespace('g', _NS)
+    ET.register_namespace('', _NS)
+
+    if len(sys.argv) < 3:
+        print("Usage: trek_cleaner.py <input_track.gpx> <output_track.gpx>")
+        sys.exit(1)
+
+    input_file_name = sys.argv[1]
+    output_file_name = sys.argv[2]
+
+    if not os.path.exists(input_file_name):
+        print("Error: File `{}` not found".format(input_file_name))
+        sys.exit(2)
+
+    tree = ET.parse(input_file_name)
+    root = tree.getroot()
+
+    # trkseg_elem - родительские элементы для точек разделённых участков трека
+    # т.е. перебираем все ветви <trgseg>...</trkseg>
+
+    ns = {'g': _NS}
+    for trkseg_elem in root.iterfind('g:trk/g:trkseg', ns):
+
+        # Найдём lat, lon и ele (если есть, иначе 0) первой точки участка
+        trkpt_prev = trkseg_elem.find('g:trkpt', ns)
+        latitude_prev = trkpt_prev.get('lat')
+        longitude_prev = trkpt_prev.get('lon')
+
+        elevation_prev = _get_elevation(trkpt_prev)
+
+        # Удалим из дерева элементов все точки, расстояние можду которыми
+        # меньше 20м. Исключение - перепад высоты больше 1м
+        first = True
+        for elem in trkseg_elem.findall('g:trkpt', ns):
+            if first:
+                # пропускаем первую точку
+                first = False
+                continue
+
+            latitude = elem.get('lat')
+            longitude = elem.get('lon')
+            elevation = _get_elevation(elem)
+
+            elevation_delta = abs(elevation - elevation_prev)
+            if elevation_delta > 1:
+                elevation_prev = elevation
+                continue
+
+            # Считаем расстояние между точками
+            mark1 = latitude_prev, longitude_prev
+            mark2 = latitude, longitude
+            distance = gd.geodesic(mark1, mark2, ellipsoid='WGS-84').m
+            if distance < _DISTANCE_TRESHOLD:
+                trkseg_elem.remove(elem)
+            else:
+                latitude_prev = latitude
+                longitude_prev = longitude
+                elevation_prev = elevation
+
+    # Записываем новый xml-документ на основе полученного дерева
+    tree.write(output_file_name, encoding="UTF-8")
+
+
+if __name__ == '__main__':
+    main()
---
+++ trek_cleaner/trek_merge.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+import xml.etree.ElementTree as ET
+from geopy import distance as gd
+
+
+_DISTANCE_TRESHOLD = 20
+
+_NS = 'http://www.topografix.com/GPX/1/1'
+
+
+def _get_elevation(point):
+    elevation_elem = point.find('{*}ele')
+    if elevation_elem is not None:
+        return float(elevation_elem.text)
+    return 0
+
+
+def main():
+    ET.register_namespace('g', _NS)
+    ET.register_namespace('', _NS)
+
+    if len(sys.argv) < 3:
+        print("Usage: trek_merge.py <input_track1.gpx> ... <input_track_n.gpx> <output_track.gpx>")
+        sys.exit(1)
+
+    main_file_name = sys.argv[1]
+    output_file_name = sys.argv[-1]
+
+    if os.path.exists(output_file_name):
+        print("Sorry, you're trying to destroy your data, please remove it first. Output file must NOT exist.")
+        sys.exit(2)
+
+    main_tree = ET.parse(main_file_name)
+    main_root = main_tree.getroot()
+    main_trk = main_root.find('g:trk')
+
+    # trkseg_elem - родительские элементы для точек разделённых участков трека
+    # т.е. перебираем все ветви <trgseg>...</trkseg>
+
+    ns = {'g': _NS}
+
+    for track_index in range(1, len(sys.argv) - 1):
+        merged_file_name = sys.argv[track_index]
+
+        merged_tree = ET.parse(merged_file_name)
+        merged_root = merged_tree.getroot()
+
+        for trkseg_elem in merged_root.iterfind('g:trk/g:trkseg', ns):
+            main_trk.append(trkseg_elem)
+
+    # Записываем новый xml-документ на основе полученного дерева
+    main_tree.write(output_file_name, encoding="UTF-8")
+
+
+if __name__ == '__main__':
+    main()
